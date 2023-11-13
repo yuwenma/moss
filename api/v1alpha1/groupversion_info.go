386aeb38957f14855e15e649d18ec1154780a048
Yuwen Ma <yuwenma@google.com>
2023-11-13 14:29:39 -0800

    add configsync
    Change-Id: I9d2053c8151fab43735da80511bee8f3fa775867

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,6 +1,6 @@
-// Package v1alpha1 contains API Schema definitions for the addons v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=addons.configdelivery.anthos.io
+// +groupName=configdelivery.anthos.io
 package v1alpha1
 
 import (
@@ -10,7 +10,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "addons.configdelivery.anthos.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "configdelivery.anthos.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"acp.git.corp.google.com/moss/controllers/argocd"
+	"acp.git.corp.google.com/moss/controllers/configsync"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
@@ -68,6 +69,13 @@ func main() {
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "ArgoCD")
+		os.Exit(1)
+	}
+	if err = (&configsync.ConfigSyncReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ConfigSync")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
